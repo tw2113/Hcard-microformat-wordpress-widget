4391aef2c8fd72744c21168a5d4acbebbc4395a4
Michael Beckwith <tw2113@gmail.com>
2019-01-18 23:33:46 -0600

    a LOT of cleanup, 8 years later

--- hcard_plugin.php
+++ hcard_plugin.php
@@ -3,150 +3,148 @@
 Plugin Name: Contact Information hcard Widget
 Plugin URI: http://michaelbox.net
 Description: Plugin that creates a widget containing optional contact information wrapped in microdata hcard classes
-Version: 1.0
+Version: 1.0.0
 Author: Michael Beckwith
 Author URI: http://michaelbox.net
+Text Domain: mbhcardwidget
 */
 
 class hCardWidget extends WP_Widget {
-	function hCardWidget() {
+
+	public function __construct() {
 		parent::WP_Widget(false, $name = 'hCardWidget');
 	}
 
-	function widget($args, $instance) {
-		//Output the widget content
+	public function widget($args, $instance) {
+
 		extract( $args );
-		/*provided by user*/
+
 		$title = apply_filters('widget_title', $instance['title']);
-		//variables to style below
-		$title = $instance['title'];
-		$first_name = $instance['first_name'];
-		$last_name = $instance['last_name'];
-		$business_name = $instance['business_name'];
+
+		$title          = $instance['title'];
+		$first_name     = $instance['first_name'];
+		$last_name      = $instance['last_name'];
+		$business_name  = $instance['business_name'];
 		$position_title = $instance['position_title'];
-		$mailbox_num = $instance['mailbox_num'];
-		$apt_suite = $instance['apt_suite'];
-		$street = $instance['street'];
-		$city = $instance['city'];
-		$state = $instance['state'];
-		$zip = $instance['zip'];
-		$email_address = $instance['email_address'];
-		$telephone = $instance['telephone'];
+		$mailbox_num    = $instance['mailbox_num'];
+		$apt_suite      = $instance['apt_suite'];
+		$street         = $instance['street'];
+		$city           = $instance['city'];
+		$state          = $instance['state'];
+		$zip            = $instance['zip'];
+		$email_address  = $instance['email_address'];
+		$telephone      = $instance['telephone'];
 
 		echo '<!--begin vcard--><div id="contact_info" class="vcard sidebarbox">';
 
-		/*Insert how to lay out the resulting information when viewing live*/
-
-		//Names area
-		if (($first_name) || ($last_name)) {
+		if ( $first_name || $last_name ) {
 			$namecheck = 'true';
 			echo '<p><span class="fn n">';
 		}
-		if ($first_name) {
-			echo '<span class="given-name">'.$first_name.'</span>';
+		if ( $first_name ) {
+			echo '<span class="given-name">' . $first_name . '</span>';
 		}
-		if ($last_name) {
-			echo '<span class="family-name"> '.$last_name.'</span>';
+		if ( $last_name ) {
+			echo '<span class="family-name"> ' . $last_name . '</span>';
 		}
-		if ($namecheck == 'true') {
+		if ( 'true' === $namecheck ) {
 			echo '</span></p>';
 		}
 
 		//Organization info area
-		if ($business_name)  {
-			echo '<span class="org">'.$business_name.'</span>';
-			if ($position_title) {//check just to see if a <br/> is needed
+		if ( $business_name )  {
+			echo '<span class="org">' . $business_name . '</span>';
+			if ( $position_title ) { //check just to see if a <br/> is needed
 				echo '<br/>';
 			}
 		}
-		if ($position_title) {
-			echo '<span class="title">'.$position_title.'</span>';
+		if ( $position_title ) {
+			echo '<span class="title">' . $position_title . '</span>';
 		}
 
 		//Address info area
-		if (($mailbox_num) || ($street) || ($city) || ($state) || ($zip) || ($email_address) || ($telephone) ) {
-		$addresscheck = 'true';
-		echo '<p class="adr">';
+		if ( $mailbox_num || $street || $city || $state || $zip || $email_address || $telephone ) {
+			$addresscheck = 'true';
+			echo '<p class="adr">';
 		}
 
-		if ($mailbox_num) {
-			echo '<span class="post-office-box">P.O. Box '.$mailbox_num.'</span><br/>';
+		if ( $mailbox_num ) {
+			echo '<span class="post-office-box">P.O. Box ' . $mailbox_num . '</span><br/>';
 		}
-		if ($street) {
-			echo '<span class="street-address">'.$street.'</span> ';
-			if ($apt_suite) {//check just to see if a <br/> is needed
+		if ( $street ) {
+			echo '<span class="street-address">' . $street . '</span>';
+			if ( $apt_suite ) {//check just to see if a <br/> is needed
 				echo '<br/>';
 			}
 		}
-		if ($apt_suite) {
-			echo '<span class="extended-address">'.$apt_suite.'</span>';
+		if ( $apt_suite ) {
+			echo '<span class="extended-address">' . $apt_suite . '</span>';
 		}
-		if ($city) {
-			echo '<span class="locality">'.$city.'</span> ';
+		if ( $city ) {
+			echo '<span class="locality">' . $city . '</span> ';
 		}
-		if ($state) {
-			echo '<span class="region">'.$state.'</span>, ';
+		if ( $state ) {
+			echo '<span class="region">' . $state . '</span>, ';
 		}
-		if ($zip) {
-			echo '<span class="postal-code">'.$zip.'</span>';
+		if ( $zip ) {
+			echo '<span class="postal-code">' . $zip . '</span>';
 		}
-		if ($addresscheck == 'true') {
-		echo '</p>';
+		if ('true' === $addresscheck ) {
+			echo '</p>';
 		}
-		if ($email_address) {
-			echo '<p><a class="email" href="mailto:'.$email_address.'">'.$email_address.'</a><br/>';
+		if ( $email_address ) {
+			echo '<p><a class="email" href="mailto:' . $email_address . '">' . $email_address . '</a><br/>';
 		}
-		if ($telephone) {
-			echo '<span class="tel">'.$telephone.'</span>';
+		if ( $telephone ) {
+			echo '<span class="tel">' . $telephone . '</span>';
 		}
 
 		echo'</div><!--end vcard-->';
 	}
 
-	function update($new_instance, $old_instance) {
-		//Process and save the widget options
+	public function update($new_instance, $old_instance) {
+
 		$instance = $old_instance;
-		/*strips tags as needed to keep them clean*/
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['link'] = strip_tags($new_instance['link']);
-		$instance['image'] = strip_tags($new_instance['image']);
-		$instance['thumb'] = strip_tags($new_instance['thumb']);
-		$instance['descrip'] = strip_tags($new_instance['descrip']);
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['first_name'] = strip_tags($new_instance['first_name']);
-		$instance['last_name'] = strip_tags($new_instance['last_name']);
-		$instance['business_name'] = strip_tags($new_instance['business_name']);
-		$instance['position_title'] = strip_tags($new_instance['position_title']);
-		$instance['mailbox_num'] = strip_tags($new_instance['mailbox_num']);
-		$instance['apt_suite'] = strip_tags($new_instance['apt_suite']);
-		$instance['street'] = strip_tags($new_instance['street']);
-		$instance['city'] = strip_tags($new_instance['city']);
-		$instance['state'] = strip_tags($new_instance['state']);
-		$instance['zip'] = strip_tags($new_instance['zip']);
-		$instance['email_address'] = strip_tags($new_instance['email_address']);
-		$instance['telephone'] = strip_tags($new_instance['telephone']);
+
+		$instance['title']          = strip_tags( $new_instance['title'] );
+		$instance['link']           = strip_tags( $new_instance['link'] );
+		$instance['image']          = strip_tags( $new_instance['image'] );
+		$instance['thumb']          = strip_tags( $new_instance['thumb'] );
+		$instance['descrip']        = strip_tags( $new_instance['descrip'] );
+		$instance['title']          = strip_tags( $new_instance['title'] );
+		$instance['first_name']     = strip_tags( $new_instance['first_name'] );
+		$instance['last_name']      = strip_tags( $new_instance['last_name'] );
+		$instance['business_name']  = strip_tags( $new_instance['business_name'] );
+		$instance['position_title'] = strip_tags( $new_instance['position_title'] );
+		$instance['mailbox_num']    = strip_tags( $new_instance['mailbox_num'] );
+		$instance['apt_suite']      = strip_tags( $new_instance['apt_suite'] );
+		$instance['street']         = strip_tags( $new_instance['street'] );
+		$instance['city']           = strip_tags( $new_instance['city'] );
+		$instance['state']          = strip_tags( $new_instance['state'] );
+		$instance['zip']            = strip_tags( $new_instance['zip'] );
+		$instance['email_address']  = strip_tags( $new_instance['email_address'] );
+		$instance['telephone']      = strip_tags( $new_instance['telephone'] );
 
 		return $instance;
 	}
 
-	function form($instance) {
+	public function form($instance) {
 		//Output the options form in admin
-		$title = $instance['title'];
-		$first_name = $instance['first_name'];
-		$last_name = $instance['last_name'];
-		$business_name = $instance['business_name'];
+		$title          = $instance['title'];
+		$first_name     = $instance['first_name'];
+		$last_name      = $instance['last_name'];
+		$business_name  = $instance['business_name'];
 		$position_title = $instance['position_title'];
-		$mailbox_num = $instance['mailbox_num'];
-		$apt_suite = $instance['apt_suite'];
-		$street = $instance['street'];
-		$city = $instance['city'];
-		$state = $instance['state'];
-		$zip = $instance['zip'];
-		$email_address = $instance['email_address'];
-		$telephone = $instance['telephone']; ?>
+		$mailbox_num    = $instance['mailbox_num'];
+		$apt_suite      = $instance['apt_suite'];
+		$street         = $instance['street'];
+		$city           = $instance['city'];
+		$state          = $instance['state'];
+		$zip            = $instance['zip'];
+		$email_address  = $instance['email_address'];
+		$telephone      = $instance['telephone']; ?>
 
-		<!--The form that the user has to enter the data-->
-			<p>Please provide at least one name field (first or last) for hcard validity. The remaining fields are optional. Output based on provided data.</p>
+			<p><?php esc_html__( 'Please provide at least one name field (first or last) for hcard validity. The remaining fields are optional. Output based on provided data.', 'mbhcardwidget' ); ?></p>
 			<p><label for="<?php echo $this->get_field_id('title'); ?>">Widget Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" placeholder="Title of the widget" type="text" value="<?php echo $title; ?>" /></label></p><!--Widget title-->
 			<p><label for="<?php echo $this->get_field_id('first_name'); ?>">First Name: <input class="widefat" id="<?php echo $this->get_field_id('first_name'); ?>" name="<?php echo $this->get_field_name('first_name'); ?>" placeholder="First name please..." type="text" value="<?php echo $first_name; ?>" /></label></p><!--First name field-->
             <p><label for="<?php echo $this->get_field_id('last_name'); ?>">Last Name: <input class="widefat" id="<?php echo $this->get_field_id('last_name'); ?>" name="<?php echo $this->get_field_name('last_name'); ?>" placeholder="...and then your last name." type="text" value="<?php echo $last_name; ?>" /></label></p><!--Last name field-->
@@ -163,8 +161,7 @@ class hCardWidget extends WP_Widget {
 
 	<?php }
 }
-//register_widget('hCardWidget');
-add_action('widgets_init', create_function('', 'return register_widget("hCardWidget");'));
 
-?>
-
+add_action( 'widgets_init', function() {
+	return register_widget( 'hCardWidget' );
+} );
